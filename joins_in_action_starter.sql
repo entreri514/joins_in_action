e509643cd04a22261c54b73bd50a177df30cf1fb
entreri514 <148565529+entreri514@users.noreply.github.com>
2023-11-12 16:08:29 -0600

    Attempted completion of problems 1-5 in joins_in_action

--- joins_in_action_starter.sql
+++ joins_in_action_starter.sql
@@ -16,22 +16,23 @@ select * from movies left join users on movie_id=favorite_movie_id;
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 2 >>>>>>>>>>>>>>>>>>>>>>>
 -- Do a right join to see all records from the users table and matching records from the movies table.
 -- EXPECTED RESULT: 16 Rows Returned
-select favorite_movie_id from users right join film;
+select film,favorite_movie_id from movies right join users on favorite_movie_id where movie_id=favorite_movie_id;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 3 >>>>>>>>>>>>>>>>>>>>>>>
 -- Do a inner join to get only the rows from the movies tables that have matching rows in the users tables.
 -- EXPECTED RESULT: 14 Rows Returned
-final_airbnbfinal_airbnb
+select movie_id,film from movies left join users on favorite_movie_id where movie_id=favorite_movie_id;
+
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 4 >>>>>>>>>>>>>>>>>>>>>>>
 -- Determine which lead studio's movies are favorited by users the most.
 -- EXPECTED RESULT: Disney
-
+select distinct lead_studio,favorite_movie_id from movies order by count(lead_studio) desc limit 1;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 5 >>>>>>>>>>>>>>>>>>>>>>>
 -- Get the average Rotten Tomatoes score of all movies that are favorited by a user.
 -- EXPECTED RESULT: 52.21
-
+select distinct rotten_tomatoes from movies order by avg(rotten_tomatoes);
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 6 >>>>>>>>>>>>>>>>>>>>>>>
 -- Determine which two movies are have duplicate entries in the dataset. (HINT: No join is needed for this problem)
